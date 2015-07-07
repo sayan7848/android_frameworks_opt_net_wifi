12ab3459d6bc75cb718fa8004c3492949d6291c4
Nalla Kartheek <karthe@codeaurora.org>
2015-07-07 13:37:47 +0530

    WiFi: Check for the status of startMonitoring before enabling p2p interface
    Framework shall initialize wlan0 interface followed by p2p0, once the
    connection to the supplicant is successful after the completion of wlan0
    initialization. This connection to the supplicant is done by invoking
    startMonitoring function during both the wlan0 and p2p0 initialization. If
    supplicant is terminated before the p2p0 start is triggered (after wlan0
    initialization), the control socket's with the supplicant shall be closed.
    This shall not trigger the stop of the p2p initialization and any
    communication to the supplicant as part of p2p start shall result in to a
    fatal exception, for the sockets getting closed. However, the
    startMonitoring API as part the p2p0 interface initialization shall know
    if the supplicant is terminated/ sockets have got closed. Thus, check if
    the status of startMonitoring is success to proceed with further p2p
    initializations.
    
    Change-Id: Iaa891d8954dc79980f61ae31f20954fec41b1237
    CRs-Fixed: 855901

--- service/java/com/android/server/wifi/WifiMonitor.java
+++ service/java/com/android/server/wifi/WifiMonitor.java
@@ -542,8 +542,8 @@ public class WifiMonitor {
         mStateMachine2 = stateMachine;
     }
 
-    public void startMonitoring() {
-        WifiMonitorSingleton.sInstance.startMonitoring(mInterfaceName);
+    public boolean startMonitoring() {
+        return WifiMonitorSingleton.sInstance.startMonitoring(mInterfaceName);
     }
 
     public void stopMonitoring() {
@@ -568,11 +568,11 @@ public class WifiMonitor {
         private WifiMonitorSingleton() {
         }
 
-        public synchronized void startMonitoring(String iface) {
+        public synchronized boolean startMonitoring(String iface) {
             WifiMonitor m = mIfaceMap.get(iface);
             if (m == null) {
                 Log.e(TAG, "startMonitor called with unknown iface=" + iface);
-                return;
+                return false;
             }
 
             Log.d(TAG, "startMonitoring(" + iface + ") with mConnected = " + mConnected);
@@ -580,7 +580,12 @@ public class WifiMonitor {
             if (mConnected) {
                 m.mMonitoring = true;
                 m.mStateMachine.sendMessage(SUP_CONNECTION_EVENT);
+                return true;
             } else {
+                if (iface.equals("p2p0")) {
+                    Log.e(TAG, "Monitoring(" + iface +") failed!, wlan0 interface restarted");
+                    return false;
+                }
                 if (DBG) Log.d(TAG, "connecting to supplicant");
                 int connectTries = 0;
                 while (true) {
@@ -589,7 +594,7 @@ public class WifiMonitor {
                         m.mStateMachine.sendMessage(SUP_CONNECTION_EVENT);
                         mConnected = true;
                         new MonitorThread(mWifiNative, this).start();
-                        break;
+                        return true;
                     }
                     if (connectTries++ < 50) {
                         try {
@@ -603,6 +608,8 @@ public class WifiMonitor {
                     }
                 }
             }
+            return false;
+
         }
 
         public synchronized void stopMonitoring(String iface) {
