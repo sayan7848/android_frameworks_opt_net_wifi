75204b294bf79022702ad4a9eefa9c6371dbfb42
Nathan Chancellor <natechancellor@gmail.com>
2018-02-21 15:40:43 -0700

    wifi: Prepare to change how the driver is loaded
    Change-Id: I3fc8e3df2d5cabe6694514c95a8567db6e803bb4
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- libwifi_hal/Android.mk
+++ libwifi_hal/Android.mk
@@ -59,9 +59,6 @@ endif
 ifdef WIFI_DRIVER_STATE_OFF
 wifi_hal_cflags += -DWIFI_DRIVER_STATE_OFF=\"$(WIFI_DRIVER_STATE_OFF)\"
 endif
-ifdef WIFI_DRIVER_LOAD_DELAY
-wifi_hal_cflags += -DWIFI_DRIVER_LOAD_DELAY
-endif
 
 # Common code shared between the HALs.
 # ============================================================
